e99cc5c644f98eedfa7da884d02b61bd6b3f2acd
liangoy <emailboxbeta@gmail.com>
2019-11-30 22:08:54 +0800

    fix

--- src/nnl/encode_and_decode.py
+++ src/nnl/encode_and_decode.py
@@ -3,37 +3,41 @@ import re
 '''
 string
 '''
+
 c1=re.compile('(\n+)')
-c2=re.compile('((?<!")(?:"")*)"(\t+)"((?:"")*(?!"))')
+c2=re.compile('(?:"")|(?:"\t+")')
 
 def _encode(t):
     t=t.replace('"','""')
     t=c1.sub('"\\1"',t).replace('\n','\t')
     return t
 
-fn=lambda x:''.join([i.replace('\t','\n')for i in x.groups()])
+def fn(matched):
+    x=matched.group(0)
+    return '"' if x=='""' else '\n'*(len(x)-2)
 
 def _decode(t):
     t=c2.sub(fn,t)
-    t=t.replace('""','"')
     return t
 
 '''
 bytes
 '''
+
 bc1=re.compile(b'(\n+)')
-bc2=re.compile(b'((?<!")(?:"")*)"(\t+)"((?:"")*(?!"))')
+bc2=re.compile(b'(?:"")|(?:"\t+")')
 
 def _bencode(t):
     t=t.replace(b'"',b'""')
     t=bc1.sub(b'"\\1"',t).replace(b'\n',b'\t')
     return t
 
-bfn=lambda x:b''.join([i.replace(b'\t',b'\n')for i in x.groups()])
+def bfn(matched):
+    x=matched.group(0)
+    return b'"' if x==b'""' else b'\n'*(len(x)-2)
 
 def _bdecode(t):
-    t=bc2.sub(bfn,t)
-    t=t.replace(b'""',b'"')
+    t=c2.sub(fn,t)
     return t
 
 '''
